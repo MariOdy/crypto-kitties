437941400abe79c65b4eb50ba33388d4f977b9b1
Marii Odynska <mvasylenko21@gmail.com>
2022-10-18 19:20:48 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface CryptoKittiesData {
+  cats: {
+    available: boolean;
+    category: string;
+    created_at: string;
+    id: number;
+    image_url: string;
+    name: string;
+    price: number;
+    updated_at: string;
+  }[];
+  pagination_info: {
+    current_page: number;
+    limit_per_page: number;
+    next_page: number;
+    prev_page: null;
+    total: number;
+    total_pages: number;
+  };
 }
+
+const App: React.FC = () => {
+  const [data, setData] = React.useState<CryptoKittiesData | null>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get<CryptoKittiesData>(
+        `https://ftl-cryptokitties.fly.dev/api/crypto_kitties`
+      );
+      setData(response.data);
+    };
+    getData();
+  }, []);
+
+  return <div className="App"></div>;
+};
 
 export default App;
