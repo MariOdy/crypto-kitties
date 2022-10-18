42864d707f9e2cd0d1813680fbabebb57a33fc5f
Marii Odynska <mvasylenko21@gmail.com>
2022-10-18 19:55:43 +0300

    Add Catalog component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Catalog from "./components/Catalog";
 
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <div></div>
+      <Catalog />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
---
+++ src/components/Catalog/Catalog.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import useCryptoKitties from "../../utils/useCryptoKitties";
+
+const Catalog: React.FC = () => {
+  const { cats, isLoading } = useCryptoKitties();
+
+  return (
+    <div className="Catalog">
+      {isLoading && <div>Loading...</div>}
+      <ul>
+        {cats?.map((cat) => (
+          <li key={cat.id}>{cat.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Catalog;
