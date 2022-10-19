57d43856c622c1651c4a94686d0488a81b8b3180
Marii Odynska <mvasylenko21@gmail.com>
2022-10-19 16:11:26 +0300

    Add color.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Catalog from "components/Catalog";
+import Header from "components/Header/Header";
 
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <Header />
       <Catalog />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+
+import "./reset.css";
+import "./index.css";
+import "./colors.css"
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
